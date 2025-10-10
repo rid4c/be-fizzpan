6af8859dc956076f507032c4c7d1f8d99309fae4
navanken <afandinaufal67@gmail.com>
2025-10-10 07:11:25 +0700

    cors deploy

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const initFunction = () => {
   const app = express();
   const PORT = 3000;
 
-  app.use(cors());
+  app.use(
+    "*",
+    cors({
+      origin: "http://localhost:5173",
+      credentials: true,
+    })
+  );
   app.use(express.json());
   app.get("/", (req, res) => {
     res.status(200).json({
